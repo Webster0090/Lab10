a159e95f216bbe1e6a3cda6a6f270fb436607c5f
Steve Webster <steve.webster190@gmail.com>
2018-10-23 22:17:00 -0400

    Add project files.

---
+++ Lab10/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace Lab10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter the radius of the circle!");
+            double userInput = double.Parse(Console.ReadLine());
+
+            Circle myCircle = new Circle(userInput);
+            Console.WriteLine($"Circumference: {myCircle.CalculateCircumference()}");
+            Console.WriteLine($"Circumference Formatted! {myCircle.CalculateFormattedCircumference()}");
+            Console.WriteLine($"Area: {myCircle.CalculateArea()}");
+            Console.WriteLine($"Area Formatted! {myCircle.CalculateFormattedArea()}");
+            Console.ReadKey();
+
+
+        }
+    }
+    class Circle
+    {
+        public const double PI = Math.PI;
+        private double radius;
+        public double Radius
+        {
+            get
+            {
+                return radius;
+            }
+            set
+            {
+                radius = value;
+            }
+        }
+
+        public Circle(double radius)
+        {
+            this.radius = radius;
+        }
+        public string FormatNumber(double x)
+        {
+            return $"{x,0:N2}";
+        }
+        public double CalculateCircumference()
+        {
+            double circumference = PI * (radius * 2);
+            return circumference;
+        } 
+        // allow for user input on number of decimal places
+        public string CalculateFormattedCircumference()
+        {
+            return FormatNumber(CalculateCircumference());
+        }
+        public double CalculateArea()
+        {
+            double area = radius * radius * PI;
+            return area;
+        }
+
+        public string CalculateFormattedArea()
+        {
+            string formattedArea = FormatNumber(CalculateArea());
+            return formattedArea;
+        }
+
+    }
+
+}
